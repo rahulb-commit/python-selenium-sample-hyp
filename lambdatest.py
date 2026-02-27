9e79be9ccc4aee908a16ff559db01721bbb185ff
rahulb-commit <rahulb@lambdatest.com>
2026-02-27 17:16:38 +0530

    Force test execution in stage mode

--- lambdatest.py
+++ lambdatest.py
@@ -12,7 +12,7 @@ class FirstSampleTest(unittest.TestCase):
 
         options = ChromeOptions()
         options.browser_version = "latest"
-        options.platform_name = "Windows 10" # Local test on Windows
+        options.platform_name = "Linux"  # Must be Linux for the HyperExecute runner 
         
         lt_options = {
             "username": username,
